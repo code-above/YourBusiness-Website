fe3d415049d4c1d9606f84d954c820c87c5e8e4c
Sonia <gillsonia91@gmail.com>
2022-12-24 10:47:14 -0500

    fix HEAD
    Signed-off-by: Sonia <gillsonia91@gmail.com>

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ module.exports = {
       PUBLIC_URL: "https://code-above.github.io/my-landing-page",
       assetPrefix: './'
     },
-    images: {
-      loader: 'default',
-      path: '',
-    },
+  
     webpack: (config, { isServer }) => {
       if (!isServer) {
         config.resolve.fallback.fs = false;
@@ -16,46 +13,6 @@ module.exports = {
       return config;
     },
   };
-  // module.exports = {
-  //   images: {
-  //     loader: 'default',
-  //     path: '',
-  //   },
-  // };
-
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-// module.exports = withPlugins([
-//   [optimizedImages, {
-//     /* config for next-optimize
-//     d-images */
-//     path:'/',
-//   }],
-
-  // your other plugins here
-
-// ]);
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       config.resolve.fallback.fs = false;
-//     }
-//     return config;
-//   },
-// };
-
-// module.exports = {
-//   images: {
-//     loader: 'akamai',
-//     path: '',
-//   },
-// };
 
 
-// module.exports = {
-//   reactStrictMode: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
+  
